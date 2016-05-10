6d8712460e405dcbe9b233926a6b4820c448c5d0
vishnuramvs <vishnuramvs@gmail.com>
2016-05-09 18:11:46 -0700

    added graph value for heartrate

--- MaterialTabs/app/src/main/java/info/androidhive/materialtabs/fragments/ThreeFragment.java
+++ MaterialTabs/app/src/main/java/info/androidhive/materialtabs/fragments/ThreeFragment.java
@@ -1,20 +1,37 @@
 package info.androidhive.materialtabs.fragments;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.eazegraph.lib.charts.ValueLineChart;
 import org.eazegraph.lib.models.ValueLinePoint;
 import org.eazegraph.lib.models.ValueLineSeries;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import info.androidhive.materialtabs.R;
 
 
 public class ThreeFragment extends Fragment{
+    public static final String MyPREFERENCES = "TokenStore" ;
 
+  static  int [] rate = new int[30];
     public ThreeFragment() {
         // Required empty public constructor
     }
@@ -22,6 +39,7 @@ public class ThreeFragment extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        new getHeartRate().execute();
     }
 
     @Override
@@ -40,8 +58,8 @@ public class ThreeFragment extends Fragment{
         series.setColor(0xFF56B7F1);
 
 
-        series.addPoint(new ValueLinePoint("1", 64f));
-        series.addPoint(new ValueLinePoint("2", 65f));
+        series.addPoint(new ValueLinePoint("1", 64));
+        series.addPoint(new ValueLinePoint("2", 65));
         series.addPoint(new ValueLinePoint("3", 54f));
         series.addPoint(new ValueLinePoint("4", 64f));
         series.addPoint(new ValueLinePoint("5", 62f));
@@ -77,6 +95,57 @@ public class ThreeFragment extends Fragment{
 
     }
 
+
+    class getHeartRate extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+
+            Context context = getActivity();
+            SharedPreferences sp = context.getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+            String code = sp.getString("access_token", "");
+
+            DateFormat df = new SimpleDateFormat("HH:mm");
+            String currentTime = df.format(Calendar.getInstance().getTime());
+
+            System.out.println(currentTime);
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet("https://api.fitbit.com/1/user/-/activities/heart/date/2016-03-01/2016-03-30.json");
+
+// Add Headers
+            httpGet.addHeader("Authorization", code);
+            //  httpGet.addHeader("key2", "value2");
+
+            try {
+
+                HttpResponse response = httpclient.execute(httpGet);
+
+                HttpEntity entity = response.getEntity();
+                String responseString = EntityUtils.toString(entity, "UTF-8");
+               // System.out.println(responseString);
+
+                JSONObject HeartRate = new JSONObject(responseString);
+
+                JSONArray RateArray = HeartRate.getJSONArray("activities-heart");
+
+                for(int i=0;i<31;i++) {
+                    rate[i] = Integer.parseInt(RateArray.getJSONObject(i).getJSONObject("value").getString("restingHeartRate"));
+
+                }
+
+                System.out.println("30 day " + rate[0]);
+
+
+
+            } catch (Exception e) {
+// TODO Auto-generated catch block
+            }
+
+            return null;
+        }
+    }
 
 
 }
